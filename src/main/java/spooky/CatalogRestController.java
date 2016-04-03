8baeef213c68298e7f9b06e3b2f948a5e5be4dda
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2016-04-03 19:14:11 +0200

    add basic error handling

--- src/main/java/spooky/CatalogRestController.java
+++ src/main/java/spooky/CatalogRestController.java
@@ -2,8 +2,13 @@ package spooky;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RefreshScope
 @RestController
@@ -20,17 +25,39 @@ public class CatalogRestController {
 
     @RequestMapping("/execute/action1")
     String executeActionOne() {
-        if(spookyFeatureConfig.isAction1Enabled()){
-            return "action ONE is enabled.";
+        if (!spookyFeatureConfig.isAction1Enabled()) {
+            throw new UnsupportedOperationException();
         }
-        return "sorry mate, no party for you.";
+
+        return "action ONE is enabled.";
     }
 
     @RequestMapping("/execute/action2")
     String executeActionTwo() {
-        if(spookyFeatureConfig.isAction2Enabled()){
-            return "action TWO is enabled.";
+        if (!spookyFeatureConfig.isAction2Enabled()) {
+            throw new UnsupportedOperationException();
         }
-        return "sorry mate, no party for you.";
+
+        return "action TWO is enabled.";
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    public void unsupportedException(HttpServletResponse response)
+            throws IOException {
+        response.sendError(
+                HttpStatus.SERVICE_UNAVAILABLE.value(),
+                "This feature is currently unavailable"
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handleGenericException(
+            HttpServletResponse response,
+            Exception e) throws IOException {
+        String msg = "There was an error processing your request: " + e.getMessage();
+        response.sendError(
+                HttpStatus.BAD_REQUEST.value(),
+                msg
+        );
     }
 }
